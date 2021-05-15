d9f589f948c9a7a96488916e845ce44e8b8838ce
urielkdev <urielkindermann@gmail.com>
2021-05-14 22:37:36 -0300

    marcos's archives

--- src/main/java/listeners/ScopeListener.java
+++ src/main/java/listeners/ScopeListener.java
@@ -1,75 +1,110 @@
-/**
- * Developed by:
- *      @João_Fellipe_Uller
- *      @Leonardo_Kreuch
- *      @Uriel_Kindermann_Caminha
- */
+package br.ufsc.grad.compilator.listeners;
 
-package main.java.listeners;
+import java.io.File;
+import java.util.HashMap;
 
-import main.antlr.MyGrammarBaseListener;
-import main.antlr.MyGrammarParser;
-import main.java.symbol_table.SymbolTable;
-import main.java.symbol_table.Scope.ScopeType;
+import org.antlr.v4.runtime.ParserRuleContext;
 
-public class ScopeListener extends MyGrammarBaseListener {
-    
-    private SymbolTable table;
+import br.ufsc.grad.compilator.Constants;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201BaseListener;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.BreakstatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.ElsestatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.ForstatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.FuncdefContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.IfstatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.OpenclosestatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.ProgramContext;
+import br.ufsc.grad.compilator.model.SymbolTable;
+import br.ufsc.grad.compilator.model.scope.ScopeType;
+
+public class ScopeListener extends ConvCCC20201BaseListener {
+
+    public SymbolTable table;
+    public HashMap<ParserRuleContext, HashMap<String, String>> customValues;
 
     public ScopeListener(SymbolTable table) {
         this.table = table;
+        customValues = new HashMap<>();
     }
 
-    @Override public void enterProgram(MyGrammarParser.ProgramContext ctx) {
+    @Override
+    public void enterProgram(ProgramContext ctx) {
+        customValues.putIfAbsent(ctx, new HashMap<>());
+        customValues.get(ctx).putIfAbsent("nextStmt", "END");
+
+        try {
+            File folder = new File(Constants.OUTPUT_PATH);
+            if (!folder.exists())
+                folder.mkdirs();
+            File outputFile = new File(Constants.SYMBOL_TABLE_OUTPUT_PATH);
+            if (outputFile.exists())
+                outputFile.delete();
+            outputFile = new File(Constants.TREE_OUTPUT_PATH);
+            if (outputFile.exists())
+                outputFile.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         table.enterScope(ScopeType.PROGRAM, ctx.start.getLine());
     }
-    
-    @Override public void exitProgram(MyGrammarParser.ProgramContext ctx) {
-        table.exitScope();
-    }
-    
-    // @Override public void enterStatelist(MyGrammarParser.StatelistContext ctx) {
-    //     table.enterScope(ScopeType.STATELIST, ctx.start.getLine());
-    // }
 
-    // @Override public void exitStatelist(MyGrammarParser.StatelistContext ctx) {
-    //     table.exitScope();
-    // }
-    
-    @Override public void enterForstat(MyGrammarParser.ForstatContext ctx) {
-        table.enterScope(ScopeType.FOR, ctx.start.getLine());
-    }
-
-    @Override public void exitForstat(MyGrammarParser.ForstatContext ctx) {
+    @Override
+    public void exitProgram(ProgramContext ctx) {
         table.exitScope();
     }
 
-    @Override public void exitBreakstat(MyGrammarParser.BreakstatContext ctx) {
+    @Override
+    public void enterOpenclosestat(OpenclosestatContext ctx) {
+        table.enterScope(ScopeType.STATELIST, ctx.start.getLine());
+    }
+
+    @Override
+    public void exitOpenclosestat(OpenclosestatContext ctx) {
+        table.exitScope();
+    }
+
+    @Override
+    public void exitBreakstat(BreakstatContext ctx) {
         if (!table.isInside(ScopeType.FOR)) {
-            throw new RuntimeException("Error: BREAK outside FOR loop in line: " + ctx.start.getLine());
+            throw new RuntimeException("Error: Break outside FOR LOOP line: " + ctx.start.getLine());
         }
     }
-    
-    @Override public void enterIfstat(MyGrammarParser.IfstatContext ctx) {
-        table.enterScope(ScopeType.IF, ctx.start.getLine());
-    }
-    
-    @Override public void enterElsestat(MyGrammarParser.ElsestatContext ctx) {
-        /* Exits IF scope. */
-        table.exitScope();
 
-        table.enterScope(ScopeType.ELSE, ctx.start.getLine());
-    }
-
-    @Override public void exitElsestat(MyGrammarParser.ElsestatContext ctx) {
-        table.exitScope();
-    }
-    
-    @Override public void enterFuncdef(MyGrammarParser.FuncdefContext ctx) {
+    @Override
+    public void enterFuncdef(FuncdefContext ctx) {
         table.enterScope(ScopeType.FUNCTION, ctx.start.getLine());
     }
 
-	@Override public void exitFuncdef(MyGrammarParser.FuncdefContext ctx) {
+    @Override
+    public void exitFuncdef(FuncdefContext ctx) {
         table.exitScope();
     }
+
+    @Override
+    public void enterIfstat(IfstatContext ctx) {
+        table.enterScope(ScopeType.IF, ctx.start.getLine());
+    }
+
+    @Override
+    public void enterElsestat(ElsestatContext ctx) {
+        table.exitScope(); // EXIT IF SCOPE
+        table.enterScope(ScopeType.ELSE, ctx.start.getLine());
+    }
+
+    @Override
+    public void exitElsestat(ElsestatContext ctx) {
+        table.exitScope();
+    }
+
+    @Override
+    public void enterForstat(ForstatContext ctx) {
+        table.enterScope(ScopeType.FOR, ctx.start.getLine());
+    }
+
+    @Override
+    public void exitForstat(ForstatContext ctx) {
+        table.exitScope();
+    }
+
 }
