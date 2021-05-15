b94b2a4376fc52826887977586d701b8c160f300
urielkdev <urielkindermann@gmail.com>
2021-05-14 22:35:04 -0300

    marcos's archives

--- src/main/java/listeners/SymbolListener.java
+++ src/main/java/listeners/SymbolListener.java
@@ -1,208 +1,110 @@
-/**
- * Developed by:
- *      @João_Fellipe_Uller
- *      @Leonardo_Kreuch
- *      @Uriel_Kindermann_Caminha
- */
+package br.ufsc.grad.compilator.listeners;
 
-package main.java.listeners;
-
+import java.io.File;
 import java.util.HashMap;
-import java.util.Optional;
 
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.Token;
 
-import main.antlr.MyGrammarBaseListener;
-import main.antlr.MyGrammarLexer;
-import main.antlr.MyGrammarParser.AllocexpressionContext;
-import main.antlr.MyGrammarParser.AtribstatContext;
-import main.antlr.MyGrammarParser.ExpressionContext;
-import main.antlr.MyGrammarParser.FuncdefContext;
-import main.antlr.MyGrammarParser.LvalueContext;
-import main.antlr.MyGrammarParser.NumexpressionContext;
-import main.antlr.MyGrammarParser.ParamlistContext;
-import main.antlr.MyGrammarParser.VardeclContext;
-import main.java.symbol_table.Symbol;
-import main.java.symbol_table.SymbolTable;
-import main.java.symbol_table.Symbol.TokenType;;
+import br.ufsc.grad.compilator.Constants;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201BaseListener;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.BreakstatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.ElsestatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.ForstatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.FuncdefContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.IfstatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.OpenclosestatContext;
+import br.ufsc.grad.compilator.antlr.ConvCCC20201Parser.ProgramContext;
+import br.ufsc.grad.compilator.model.SymbolTable;
+import br.ufsc.grad.compilator.model.scope.ScopeType;
 
-public class SymbolListener extends MyGrammarBaseListener {
-    
-    private SymbolTable table;
-    private HashMap<ParserRuleContext, String> types;
+public class ScopeListener extends ConvCCC20201BaseListener {
 
-    public SymbolListener(SymbolTable table) {
+    public SymbolTable table;
+    public HashMap<ParserRuleContext, HashMap<String, String>> customValues;
+
+    public ScopeListener(SymbolTable table) {
         this.table = table;
-        this.types = new HashMap<>();
+        customValues = new HashMap<>();
     }
 
     @Override
-    public void exitParamlist(ParamlistContext ctx) {
-        if (ctx.Ident() != null) {
-            Token varType = ctx.vartype().start;
-            Token varName = ctx.Ident().getSymbol();
-            
-            table.addSymbol(varName, getFromWord(varType.getText()));
+    public void enterProgram(ProgramContext ctx) {
+        customValues.putIfAbsent(ctx, new HashMap<>());
+        customValues.get(ctx).putIfAbsent("nextStmt", "END");
+
+        try {
+            File folder = new File(Constants.OUTPUT_PATH);
+            if (!folder.exists())
+                folder.mkdirs();
+            File outputFile = new File(Constants.SYMBOL_TABLE_OUTPUT_PATH);
+            if (outputFile.exists())
+                outputFile.delete();
+            outputFile = new File(Constants.TREE_OUTPUT_PATH);
+            if (outputFile.exists())
+                outputFile.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        table.enterScope(ScopeType.PROGRAM, ctx.start.getLine());
+    }
+
+    @Override
+    public void exitProgram(ProgramContext ctx) {
+        table.exitScope();
+    }
+
+    @Override
+    public void enterOpenclosestat(OpenclosestatContext ctx) {
+        table.enterScope(ScopeType.STATELIST, ctx.start.getLine());
+    }
+
+    @Override
+    public void exitOpenclosestat(OpenclosestatContext ctx) {
+        table.exitScope();
+    }
+
+    @Override
+    public void exitBreakstat(BreakstatContext ctx) {
+        if (!table.isInside(ScopeType.FOR)) {
+            throw new RuntimeException("Error: Break outside FOR LOOP line: " + ctx.start.getLine());
         }
     }
 
     @Override
-    public void exitVardecl(VardeclContext ctx) {
-        Optional<Symbol> optional;
-        Token varType = ctx.vartype().start;
-        Token varName = ctx.Ident().getSymbol();
-        int line = ctx.getStart().getLine();
-
-        optional = table.findSymbol(varName.getText());
-
-        if (!optional.isEmpty()){
-            throw new RuntimeException("Declaration error in line " + line + ": \"" + varName.getText() + "\" already declared.");
-        } else {
-            table.addSymbol(varName, getFromWord(varType.getText()));
-        }
+    public void enterFuncdef(FuncdefContext ctx) {
+        table.enterScope(ScopeType.FUNCTION, ctx.start.getLine());
     }
 
     @Override
     public void exitFuncdef(FuncdefContext ctx) {
-        Optional<Symbol> optional;
-        Token varName = ctx.Ident().getSymbol();
-        int line = ctx.getStart().getLine();
-
-        optional = table.findSymbol(varName.getText());
-
-        if (!optional.isEmpty()){
-            throw new RuntimeException("Declaration error in line " + line + ": \"" + varName.getText() + "\" already declared.");
-        } else {
-            table.addSymbol(varName, TokenType.FUNCTION);
-        }
+        table.exitScope();
     }
 
     @Override
-    public void exitAtribstat(AtribstatContext ctx) {
-        String varName = ctx.lvalue().Ident().getText();
-        Optional<Symbol> optional = table.findSymbol(varName);
-        Optional<Symbol> opt2;
-        TokenType t1, t2;
-        Token lastT;
-
-        if (optional.isEmpty()) {
-            throw new RuntimeException("Error: Undeclared variable \"" + varName + "\" in line " + ctx.start.getLine());
-        } else {
-            Symbol token = optional.get();
-            t1 = token.getType();
-
-            /* Is it a regular atrib? */
-            if (ctx.atribstataux().expression() == null) {
-                lastT = ctx.atribstataux().allocexpression().vartype().start;
-                t2 = getFromWord(lastT.getText());
-            } else {
-                lastT = ctx.atribstataux().expression().start;
-
-                if (lastT != null) {
-                    /* Is it an identifier? */
-                    if (lastT.getType() == MyGrammarLexer.Ident) {
-                        opt2 = table.findSymbol(lastT.getText());
-
-                        t2 = opt2.get().getType();
-                    } else {
-                        t2 = getFromTokenType(lastT.getType());
-                    }
-                }
-                else
-                    throw new RuntimeException("Error: Unknown error in line " + ctx.start.getLine());
-            }
-
-            /* Incompatible types? */
-            if (t1 != t2) {
-                throw new RuntimeException("Error: Incompatible types in expression in line " + ctx.start.getLine() +
-                    ":\nTrying to attrib " + t2 + " to " + t1 + "(" + token.getLexeme() + ")");
-            }
-        }
+    public void enterIfstat(IfstatContext ctx) {
+        table.enterScope(ScopeType.IF, ctx.start.getLine());
     }
 
     @Override
-    public void exitLvalue(LvalueContext ctx) {
-        Optional<Symbol> optional = table.findSymbol(ctx.Ident().getText());
-        if(optional.isEmpty())
-        {
-            System.out.println("Teste");
-            throw new RuntimeException("Error: Undeclared identifier \"" + ctx.Ident().getText() + "\" in line " + ctx.start.getLine());
-        }
+    public void enterElsestat(ElsestatContext ctx) {
+        table.exitScope(); // EXIT IF SCOPE
+        table.enterScope(ScopeType.ELSE, ctx.start.getLine());
     }
 
     @Override
-    public void exitAllocexpression(AllocexpressionContext ctx) {
-        types.put(ctx, ctx.children.get(1).getText());
+    public void exitElsestat(ElsestatContext ctx) {
+        table.exitScope();
     }
 
     @Override
-    public void exitExpression(ExpressionContext ctx) { }
+    public void enterForstat(ForstatContext ctx) {
+        table.enterScope(ScopeType.FOR, ctx.start.getLine());
+    }
 
     @Override
-    public void exitNumexpression(NumexpressionContext ctx) {
-        Token firstTerm, secondTerm;
-        TokenType ftType, stType;
-
-        /* Is this the last term of the expression? */
-        if (ctx.numexpressionaux().getChildCount() == 0)
-            return;
-
-        firstTerm = ctx.term().start;
-        secondTerm = ctx.numexpressionaux().numexpression().term().start;
-
-        ftType = getTermType(firstTerm, ctx);
-        stType = getTermType(secondTerm, ctx);
-
-        /* Same type? */
-        if (ftType != stType)
-            throw new RuntimeException("Error: Incompatible types in expression in line " + ctx.start.getLine() +
-                ":\nTrying to operate " + ftType + "(" + firstTerm.getText() + ")" + " with " + stType + "(" + secondTerm.getText() + ")");
+    public void exitForstat(ForstatContext ctx) {
+        table.exitScope();
     }
 
-    public TokenType getTermType(Token token, NumexpressionContext ctx) {
-        Optional<Symbol> symbol;
-
-        if (token.getType() == MyGrammarLexer.Ident) {
-            symbol = table.findSymbol(token.getText());
-
-            if (symbol.isEmpty())
-            {
-                throw new RuntimeException("Error: Undeclared identifier \"" + token.getText() + "\" in line " + ctx.start.getLine());
-            }
-
-            return (symbol.get().getType());
-        }
-        else {
-            return (getFromTokenType(token.getType()));
-        }
-    }
-
-    public TokenType getFromTokenType(int type) {
-        switch (type) {
-            case MyGrammarLexer.Int_constant:
-                return (TokenType.INT);
-            case MyGrammarLexer.Float_constant:
-                return (TokenType.FLOAT);
-            case MyGrammarLexer.String_constant:
-                return (TokenType.STRING);
-            default:
-                return (null);
-        }
-    }
-
-    public TokenType getFromWord(String type) {
-        switch (type) {
-            case "int":
-                return (TokenType.INT);
-            case "float":
-                return (TokenType.FLOAT);
-            case "string":
-                return (TokenType.STRING);
-            case "function":
-                return (TokenType.FUNCTION);
-            default:
-                return (null);
-        }
-    }
 }
